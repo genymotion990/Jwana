605d4f8278d9fd933938414736e30eab7bf67bc2
jwanawad <jwana.awaad@gmail.com>
2017-10-02 13:41:46 +0300

    first commit

--- app/src/main/java/com/example/hp1/project/Disneyland.java
+++ app/src/main/java/com/example/hp1/project/Disneyland.java
@@ -2,6 +2,10 @@ package com.example.hp1.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static com.example.hp1.project.R.id.textView;
 
 public class Disneyland extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class Disneyland extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disneyland);
+
     }
+
 }
