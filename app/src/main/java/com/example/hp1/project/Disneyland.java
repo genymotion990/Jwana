eb0e023c8f233c1cb28b3cbdc1fac9cdd5507a03
jwanawad <jwana.awaad@gmail.com>
2017-10-02 12:32:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hp1/project/Disneyland.java
@@ -0,0 +1,13 @@
+package com.example.hp1.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Disneyland extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disneyland);
+    }
+}
---
+++ app/src/main/java/com/example/hp1/project/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.hp1.project;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static android.content.Intent.ACTION_VIEW;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+ListView lv1;
+    ArrayAdapter<String> adapter;
+    ArrayList <String> California=new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        California.add("Universal Studio Hollywood");
+        California.add("Disney Land");
+        California.add("Lego Land");
+        California.add("Hollywood Wax Museum");
+        California.add("About The Application");
+        adapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,California);
+        lv1=(ListView)findViewById(R.id.lv1);
+        lv1.setAdapter(adapter);
+lv1.setOnItemClickListener(this);
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if(position==0){
+            Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("https://promotions.universalstudioshollywood.com/general-admission-ticket-usa/?__source=search_gps_all-lang_b_intl_intl_busa_brand_tix_15off&gclid=Cj0KEQjwzpfHBRC1iIaL78Ol-eIBEiQAdZPVKmivP0ut8SdRKdjOzPBrJ3fK5smirpJb8GiXcPDZICMaAm428P8HAQ"));
+            startActivity(i);
+        }
+        if (position==1){
+            Intent i1=new Intent(this,Disneyland.class);
+            startActivity(i1);
+        }
+        if (position==2){
+            Intent i2=new Intent(this,Legoland.class);
+            startActivity(i2);
+        }
+        if(position==3){
+            Intent i3=new Intent(this,Hollywood.class);
+            startActivity(i3);
+        }
+        if(position==4){
+            Intent i4=new Intent(this,About.class);
+            startActivity(i4);
+        }
+
+    }
+}
